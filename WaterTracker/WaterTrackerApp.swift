0b7e3a66d53dbd5193849dc873d5003107015a72
Michael Trainin <mike@Michaels-Air.lan>
2025-04-11 13:04:37 -0500

    Initial project

---
+++ WaterTracker/WaterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackerApp.swift
+//  WaterTracker
+//
+//  Created by Michael Trainin on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
